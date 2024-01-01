5dbf0810000c8b8ac1bf4a4a7d08e2042fa390dc
manong <wzy871@outlook.com>
2024-01-01 19:10:15 +0800

    资源管理

---
+++ bage-finance-biz/src/main/java/com/bage/finance/biz/mapper/SysResourceMapper.java
@@ -0,0 +1,9 @@
+package com.bage.finance.biz.mapper;
+
+import com.bage.finance.biz.domain.SysResource;
+import com.bage.mybatis.help.CommonMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SysResourceMapper extends CommonMapper<SysResource> {
+}
