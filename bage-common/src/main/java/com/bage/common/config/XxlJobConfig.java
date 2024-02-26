8767344babd168aeb3c2207b6ceb55bfebbd108a
manong <wzy871@outlook.com>
2024-02-27 01:14:56 +0800

    单元测试优化

--- bage-common/src/main/java/com/bage/common/config/XxlJobConfig.java
+++ bage-common/src/main/java/com/bage/common/config/XxlJobConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @ConfigurationProperties(prefix = "xxl.job")
+@ConditionalOnProperty("xxl.job")
 @Slf4j
 public class XxlJobConfig {
     @Value("${xxl.job.admin.addresses}")
