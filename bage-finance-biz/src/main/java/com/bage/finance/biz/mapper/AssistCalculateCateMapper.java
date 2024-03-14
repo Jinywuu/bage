d2b9341a98fb3c302db5b1c6bf1c360cffc5e4e5
manong <wzy871@outlook.com>
2024-03-14 23:56:28 +0800

    辅助核算类别管理

---
+++ bage-finance-biz/src/main/java/com/bage/finance/biz/mapper/AssistCalculateCateMapper.java
@@ -0,0 +1,9 @@
+package com.bage.finance.biz.mapper;
+
+import com.bage.finance.biz.domain.AssistCalculateCate;
+import com.bage.mybatis.help.CommonMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AssistCalculateCateMapper extends CommonMapper<AssistCalculateCate> {
+}
