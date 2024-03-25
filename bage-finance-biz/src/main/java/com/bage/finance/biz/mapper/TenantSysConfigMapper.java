17ead4bb9b8eb36c1b1e5731a6b943ca9b551d9a
manong <wzy871@outlook.com>
2024-03-26 03:16:53 +0800

    科目管理

---
+++ bage-finance-biz/src/main/java/com/bage/finance/biz/mapper/TenantSysConfigMapper.java
@@ -0,0 +1,9 @@
+package com.bage.finance.biz.mapper;
+
+import com.bage.finance.biz.domain.TenantSysConfig;
+import com.bage.mybatis.help.CommonMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TenantSysConfigMapper extends CommonMapper<TenantSysConfig> {
+}
