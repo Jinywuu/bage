a8b01765dd07b92cc31cdf9d78d6fa0616be8d88
manong <wzy871@outlook.com>
2024-04-03 12:02:54 +0800

    docker配置相关

--- bage-finance-biz/src/main/java/com/bage/finance/biz/service/impl/AssistCalculateSummaryServiceImpl.java
+++ bage-finance-biz/src/main/java/com/bage/finance/biz/service/impl/AssistCalculateSummaryServiceImpl.java
@@ -283,7 +283,8 @@ public class AssistCalculateSummaryServiceImpl implements AssistCalculateSummary
         transactionTemplate.execute((transactionStatus) -> {
             create(assistCalculateSummary);
             form.setAssistCalculateSummaryId(assistCalculateSummary.getId());
-            AssistCalculateHandleService assistCalculateHandleService = assistCalculateHandlerFactory.get(assistCalculateSummary.getAssistCalculateCateCode());
+            AssistCalculateHandleService assistCalculateHandleService = assistCalculateHandlerFactory
+                    .get(assistCalculateSummary.getAssistCalculateCateCode());
             assistCalculateHandleService.create(form);
             if (!assistCalculateCateService.addUseCount(form.getAssistCalculateCateId(), 1)) {
                 throw new BizException("创建失败");
